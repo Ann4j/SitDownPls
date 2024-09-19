87eb900eb07517f1d8fcd90f32719238ec2d817c
Анна Чернышева <annacernyseva@MacBook-Air-Anna-2.local>
2024-09-19 19:39:29 +0300

    catalog, card product

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,11 @@
 const path = require('path');
 const PugPlugin = require('pug-plugin');
+const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin');
 
 const mode = process.env.NODE_ENV === 'production' ? 'production' : 'development';
 
 module.exports = {
   mode,
-
   output: {
     path: path.join(__dirname, 'dist/'),
     assetModuleFilename: "assets/[hash][ext][query]",
@@ -14,7 +14,9 @@ module.exports = {
 
   entry: {
     // define Pug files here
-    index: './src/index.pug', // => dist/index.html
+    index: './src/index.pug',
+    catalog: './src/catalog.pug',
+    productCard: './src/productCard.pug'
   },
 
   plugins: [
@@ -46,6 +48,35 @@ module.exports = {
         test: /\.(woff|woff2|eot|ttf|otf|)$/i,
         type: 'asset/resource',
       },
+    ],
+  },
+
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    open: true, // Открывает браузер при запуске
+    compress: true,
+    port: 9000, // Порт, который ты хочешь использовать
+    devMiddleware: {
+      index: 'index.html', // Открыть catalog.html вместо index.html
+    },
+  },
+
+  optimization: {
+    minimizer: [
+      new ImageMinimizerPlugin({
+        minimizer: {
+          implementation: ImageMinimizerPlugin.imageminMinify,
+          options: {
+            plugins: [
+              ['jpegtran', { progressive: true }],
+              ['optipng', { optimizationLevel: 5 }],
+              ['pngquant', { quality: [0.65, 0.80], speed: 4 }],
+            ],
+          },
+        },
+      }),
     ],
   },
 };
